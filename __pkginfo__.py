2feb5d5efe0e44b7c677a4774ea999a05182ea84
Sylvain Thénault <sylvain.thenault@logilab.fr>
2006-11-06 17:20:08 +0100

    normalize distname

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -20,6 +20,7 @@ logilab.astng packaging information
 """
 
 modname = 'astng'
+distname = 'logilab-astng'
 numversion = (0, 16, 1)
 version = '.'.join([str(num) for num in numversion])
 pyversions = ["2.3", "2.4", "2.5"]
