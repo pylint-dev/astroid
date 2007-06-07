26ba7aab420892e1f03f3838b1c5b67cc9f99a91
Sylvain Thénault <sylvain.thenault@logilab.fr>
2007-06-07 16:31:41 +0200

    prepare 0.17.1

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -21,7 +21,7 @@ logilab.astng packaging information
 
 modname = 'astng'
 distname = 'logilab-astng'
-numversion = (0, 17, 0)
+numversion = (0, 17, 1)
 version = '.'.join([str(num) for num in numversion])
 pyversions = ["2.3", "2.4", "2.5"]
 
