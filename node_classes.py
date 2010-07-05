c85af9cce2a3115da3dfaa45d61e3be2d1f51ab4
Sylvain Thénault <sylvain.thenault@logilab.fr>
2010-07-05 08:10:02 +0200

    copyright cleanup
    --HG--
    branch : stable

--- node_classes.py
+++ node_classes.py
@@ -1,16 +1,3 @@
-# -*- coding: utf-8 -*-
-# This program is free software; you can redistribute it and/or modify it under
-# the terms of the GNU Lesser General Public License as published by the Free Software
-# Foundation; either version 2 of the License, or (at your option) any later
-# version.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-# FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public License along with
-# this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 # copyright 2003-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 # copyright 2003-2010 Sylvain Thenault, all rights reserved.
@@ -30,11 +17,9 @@
 #
 # You should have received a copy of the GNU Lesser General Public License along
 # with logilab-astng. If not, see <http://www.gnu.org/licenses/>.
+"""Module for some node classes. More nodes in scoped_nodes.py
 """
-Module for some node classes. More nodes in scoped_nodes.py
 
-
-"""
 from logilab.common.compat import chain, imap
 
 from logilab.astng import NoDefault
@@ -55,7 +40,6 @@ def unpack_infer(stmt, context=None):
     if infered is stmt:
         return iter( (stmt,) )
     return chain(*imap(unpack_infer, stmt.infer(context)))
-
 
 
 def are_exclusive(stmt1, stmt2, exceptions=None):
