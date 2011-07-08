d6944a6ed9f7e452b2eb079269d212816c6f2f10
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-07-08 17:03:47 +0200

    cleanups

--- mixins.py
+++ mixins.py
@@ -108,7 +108,7 @@ class FromImportMixIn(FilterStmtsMixin):
         # XXX: no more needed ?
         mymodule = self.root()
         level = getattr(self, 'level', None) # Import as no level
-        # XXX we should investigate deeper if we really want to check 
+        # XXX we should investigate deeper if we really want to check
         # importing itself: modname and mymodule.name be relative or absolute
         if mymodule.relative_to_absolute_name(modname, level) == mymodule.name:
             # FIXME: we used to raise InferenceError here, but why ?
