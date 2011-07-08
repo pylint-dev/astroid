abab1061c29b5f1d803128392bf61768eabaf304
Sylvain Thénault <sylvain.thenault@logilab.fr>
2011-07-08 15:55:38 +0200

    fix absolute import behaviour with python 2.7: it seems that absolute import are actually not activated by default, at least with 2.7.1

--- scoped_nodes.py
+++ scoped_nodes.py
@@ -1,4 +1,4 @@
-# copyright 2003-2010 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
+# copyright 2003-2011 LOGILAB S.A. (Paris, FRANCE), all rights reserved.
 # contact http://www.logilab.fr/ -- mailto:contact@logilab.fr
 # copyright 2003-2010 Sylvain Thenault, all rights reserved.
 # contact mailto:thenault@gmail.com
@@ -313,7 +313,7 @@ class Module(LocalsDictNodeNG):
         """module has no sibling"""
         return
 
-    if sys.version_info < (2, 7):
+    if sys.version_info < (2, 8):
         def absolute_import_activated(self):
             for stmt in self.locals.get('absolute_import', ()):
                 if isinstance(stmt, From) and stmt.modname == '__future__':
