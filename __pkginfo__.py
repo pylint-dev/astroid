58bb9c4ddbd26c179f08444cb4286d627e50b47c
Sylvain Thénault <sylvain.thenault@logilab.fr>
2012-10-05 12:52:17 +0200

    0.24.1
    --HG--
    branch : stable

--- __pkginfo__.py
+++ __pkginfo__.py
@@ -24,7 +24,7 @@ distname = 'logilab-astng'
 modname = 'astng'
 subpackage_of = 'logilab'
 
-numversion = (0, 24, 0)
+numversion = (0, 24, 1)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = ['logilab-common >= 0.53.0']
