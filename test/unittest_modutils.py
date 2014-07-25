1a4198ea9b1d5ad9f1eef59f8f3639f098f68e6b
Claudiu Popa <pcmanticore@gmail.com>
2014-07-25 10:23:16 +0200

    Skip a test on Windows and Python 3.

--- test/unittest_modutils.py
+++ test/unittest_modutils.py
@@ -196,8 +196,10 @@ class is_standard_module_tc(TestCase):
 
     def test_4(self):
         import astroid
-        if sys.version_info > (3, 0) and '.tox' in astroid.__file__:
-            self.skipTest('py3 tox environments are known to have bad STD_LIB_DIR, breaking this test')
+        if (sys.version_info > (3, 0) and sys.platform.startswith('win')):
+            self.skipTest('imp module has a broken behaviour in Python 3 on '
+                          'Windows, returning the module path with different '
+                          'case than it should be.')
         self.assertEqual(modutils.is_standard_module('hashlib'), True)
         self.assertEqual(modutils.is_standard_module('pickle'), True)
         self.assertEqual(modutils.is_standard_module('email'), True)
