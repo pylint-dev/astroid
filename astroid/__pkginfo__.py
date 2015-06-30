236111323d59b98d3b1a231296273c7d2dfc239a
Claudiu Popa <cpopa@cloudbasesolutions.com>
2015-07-01 02:44:43 +0300

    Pin dependency of logilab.common to 0.63.2 for now.

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -23,7 +23,7 @@ modname = 'astroid'
 numversion = (1, 3, 6)
 version = '.'.join([str(num) for num in numversion])
 
-install_requires = ['logilab-common >= 0.60.0', 'six']
+install_requires = ['logilab-common==0.63.2', 'six']
 
 license = 'LGPL'
 
