809c9eaab13b3ad17dc662e1f18946f7226fe924
Rene Zhang <rz99@cornell.edu>
2015-08-20 17:39:19 -0700

    Fix infinite loop in metaclass inference (grafted from 43857b00bf3e6f2f93358f7ea89a7588828045c0)
    --HG--
    branch : 1.4.0

--- astroid/scoped_nodes.py
+++ astroid/scoped_nodes.py
@@ -1388,19 +1388,24 @@ class ClassDef(mixins.FilterStmtsMixin, LocalsDictNodeNG, bases.Statement):
             return None
         return inferred
 
-    def metaclass(self):
+    def metaclass(self, seen=None):
         """Return the metaclass of this class.
 
         If this class does not define explicitly a metaclass,
         then the first defined metaclass in ancestors will be used
         instead.
         """
+        if seen is None:
+            seen = set()
+        seen.add(self)
+
         klass = self._explicit_metaclass()
         if klass is None:
             for parent in self.ancestors():
-                klass = parent.metaclass()
-                if klass is not None:
-                    break
+                if parent not in seen:
+                    klass = parent.metaclass(seen=seen)
+                    if klass is not None:
+                        break
         return klass
 
     def has_metaclass_hack(self):
