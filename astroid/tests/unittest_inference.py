6e693aa45827f8078f1c04f9a89f546e39ec8a53
Claudiu Popa <pcmanticore@gmail.com>
2015-10-08 01:07:09 +0300

    Make duplicated_kwargs a public API.
    --HG--
    branch : 1.4.0

--- astroid/tests/unittest_inference.py
+++ astroid/tests/unittest_inference.py
@@ -2050,6 +2050,11 @@ class CallSiteTest(unittest.TestCase):
 
 
 
+    def test_duplicated_keyword_arguments(self):
+        ast_node = test_utils.extract_node('f(f=24, **{"f": 25})')
+        site = self._call_site_from_call(ast_node)
+        self.assertIn('f', site.duplicated_keywords)
+
 
 if __name__ == '__main__':
     unittest.main()
