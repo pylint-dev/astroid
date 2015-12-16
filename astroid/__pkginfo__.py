f40f50f3cd60c6d5c8e3202e5a8a47e07f864ebc
Claudiu Popa <pcmanticore@gmail.com>
2015-12-16 17:45:48 +0200

    master astroid is now 1.6.0

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -22,7 +22,7 @@ distname = 'astroid'
 
 modname = 'astroid'
 
-numversion = (1, 5, 0)
+numversion = (1, 6, 0)
 version = '.'.join([str(num) for num in numversion])
 
 install_requires = ['lazy_object_proxy', 'six', 'wrapt']
