b660d0c88a7e72049ee1299b0bcef84345626d0d
Claudiu Popa <pcmanticore@gmail.com>
2016-01-14 23:11:24 +0200

    Support accessing properties using super().

--- astroid/objects.py
+++ astroid/objects.py
@@ -173,6 +173,10 @@ class Super(node_classes.NodeNG):
                     yield inferred
                 elif self._class_based or inferred.type == 'staticmethod':
                     yield inferred
+                elif bases._is_property(inferred):
+                    # TODO: support other descriptors as well.
+                    for value in inferred.infer_call_result(self, context):
+                        yield value
                 else:
                     yield bases.BoundMethod(inferred, cls)
 
