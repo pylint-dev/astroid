c61b992458e1b1b3d955abf11ac3b75c98a11503
Claudiu Popa <pcmanticore@gmail.com>
2016-07-16 14:17:16 +0300

    Make Uninferable have a false value by default.

--- astroid/util.py
+++ astroid/util.py
@@ -50,6 +50,11 @@ class Uninferable(object):
     def __call__(self, *args, **kwargs):
         return self
 
+    def __bool__(self):
+        return False
+
+    __nonzero__ = __bool__
+
     def accept(self, visitor):
         func = getattr(visitor, "visit_uninferable")
         return func(self)
