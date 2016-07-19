153ba40f10e026fd3ec936b447147cff873a535a
Claudiu Popa <pcmanticore@gmail.com>
2016-07-19 19:17:40 +0300

    Keep a consistent copyright notice across the board.

--- astroid/brain/brain_namedtuple_enum.py
+++ astroid/brain/brain_namedtuple_enum.py
@@ -1,3 +1,5 @@
+# Copyright (c) 2012-2016 LOGILAB S.A. (Paris, FRANCE)
+# http://www.logilab.fr/ -- mailto:contact@logilab.fr
 # Licensed under the LGPL: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
 # For details: https://github.com/PyCQA/astroid/blob/master/COPYING.LESSER
 
