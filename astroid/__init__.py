5a3b55258c3674a8f44c0bdc30c7516ceffd74eb
Moisés López <moylop260@vauxoo.com>
2016-08-22 14:14:06 -0500

    Export __version__ in __init__.

--- astroid/__init__.py
+++ astroid/__init__.py
@@ -29,6 +29,7 @@ Main modules are:
 
 * builder contains the class responsible to build astroid trees
 """
+
 import os
 import sys
 import re
@@ -45,6 +46,7 @@ Del = _Context.Del
 del _Context
 
 
+from .__pkginfo__ import version as __version__
 # WARNING: internal imports order matters !
 
 # pylint: disable=redefined-builtin, wildcard-import
--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -10,8 +10,8 @@ distname = 'astroid'
 
 modname = 'astroid'
 
-numversion = (1, 5, 0)
-version = '.'.join([str(num) for num in numversion])
+version = '1.5.0'
+numversion = tuple(map(int, version.split('.')))
 
 extras_require = {}
 install_requires = ['lazy_object_proxy', 'six', 'wrapt']
