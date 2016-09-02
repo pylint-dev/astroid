9979615030d97cbe26bb64e005f7dd83012f013d
Moises Lopez - https://www.vauxoo.com/ <moylop260@vauxoo.com>
2016-09-02 02:36:21 -0500

    Add compatibility with old versions of setuptools (#360)
    Ranged environment markers can be used only in recent versions of setuptools. Instead of relying on extras_require, we are falling back to install_requires when we detect an incompatibility between the installed setuptools version and what astroid expects.

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -6,6 +6,12 @@
 # For details: https://github.com/PyCQA/astroid/blob/master/COPYING.LESSER
 
 """astroid packaging information"""
+
+from sys import version_info as py_version
+
+from pkg_resources import parse_version
+from setuptools import __version__ as setuptools_version
+
 distname = 'astroid'
 
 modname = 'astroid'
@@ -15,7 +21,23 @@ numversion = tuple(map(int, version.split('.')))
 
 extras_require = {}
 install_requires = ['lazy_object_proxy', 'six', 'wrapt']
-extras_require[':python_version<"3.4"'] = ['enum34', 'singledispatch']
+
+
+def has_environment_marker_range_operators_support():
+    """Code extracted from 'pytest/setup.py'
+    https://github.com/pytest-dev/pytest/blob/7538680c/setup.py#L31
+
+    The first known release to support environment marker with range operators
+    it is 17.1, see: https://setuptools.readthedocs.io/en/latest/history.html#id113
+    """
+    return parse_version(setuptools_version) >= parse_version('17.1')
+
+
+if has_environment_marker_range_operators_support():
+    extras_require[':python_version<"3.4"'] = ['enum34', 'singledispatch']
+elif py_version < (3, 4):
+    install_requires.extend(['enum34', 'singledispatch'])
+
 
 # pylint: disable=redefined-builtin; why license is a builtin anyway?
 license = 'LGPL'
