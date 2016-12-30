ef05be71134054abaa3aa68e4c7ea67d701e5c13
Łukasz Rogalski <rogalski.91@gmail.com>
2016-12-30 17:18:19 +0100

    Improve inference of OrderedDict (#381)

--- astroid/brain/brain_collections.py
+++ astroid/brain/brain_collections.py
@@ -32,6 +32,8 @@ def _collections_transform():
         def __getitem__(self, index): pass
         def __setitem__(self, index, value): pass
         def __delitem__(self, index): pass
+
+    class OrderedDict(dict): pass
     ''')
 
 
