efea818ebaed016dde53dd2db16a488028a66196
Łukasz Rogalski <rogalski.91@gmail.com>
2017-01-26 20:38:50 +0100

    Fix once again

--- astroid/tests/testdata/data/module.py
+++ astroid/tests/testdata/data/module.py
@@ -69,12 +69,6 @@ class YOUPI(YO):
         pass
     class_method = classmethod(class_method)
 
-    def __init_subclass__(cls, **kwargs):
-        """This methid is implicitly classmethod in python3.6+
-        See: https://www.python.org/dev/peps/pep-0487
-        """
-        pass
-
 
 def four_args(a, b, c, d):
     """four arguments (was nested_args)"""
