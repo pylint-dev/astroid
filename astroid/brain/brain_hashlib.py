1ef8a417cd0a4b26284cc0fed17771a51d558018
Claudiu Popa <pcmanticore@gmail.com>
2018-03-28 09:36:51 +0200

    Add blake2b to the list of hashlib members. Close PyCQA/pylint#1968

--- astroid/brain/brain_hashlib.py
+++ astroid/brain/brain_hashlib.py
@@ -34,7 +34,8 @@ def _hashlib_transform():
     algorithms = ['md5', 'sha1', 'sha224', 'sha256', 'sha384', 'sha512']
     if PY36:
         algorithms += [
-            'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512', 'shake_128', 'shake_256'
+            'sha3_224', 'sha3_256', 'sha3_384', 'sha3_512', 'shake_128', 'shake_256',
+            'blake2b',
         ]
     classes = "".join(
         template % {'name': hashfunc, 'digest': 'b""' if six.PY3 else '""'}
