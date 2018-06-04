be21add2506049b0fa3622e8404158fe8a597e6d
Claudiu Popa <pcmanticore@gmail.com>
2018-06-04 08:49:16 -0700

    Allow google style docs to be built along side sphinx style docs, using napoleon extension

--- doc/conf.py
+++ doc/conf.py
@@ -33,6 +33,7 @@ extensions = [
     'sphinx.ext.intersphinx',
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
