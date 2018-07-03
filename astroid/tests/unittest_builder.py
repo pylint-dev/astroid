9cc0b8a1275418f51fb305c7da1be938abad3e4e
Claudiu Popa <pcmanticore@gmail.com>
2018-07-03 13:07:47 +0200

    Fix lint

--- astroid/tests/unittest_builder.py
+++ astroid/tests/unittest_builder.py
@@ -509,6 +509,7 @@ class BuilderTest(unittest.TestCase):
             '''
         builder.parse(code)
         nonetype = nodes.const_factory(None)
+        # pylint: disable=no-member; Infers two potential values
         self.assertNotIn('custom_attr', nonetype.locals)
         self.assertNotIn('custom_attr', nonetype.instance_attrs)
         nonetype = nodes.const_factory({})
