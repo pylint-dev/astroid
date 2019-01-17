964cbb76a3e3af32af297b717a613a0c7df94d07
Claudiu Popa <pcmanticore@gmail.com>
2019-01-17 15:28:05 +0100

    Disable test on Python 3.4

--- astroid/tests/unittest_brain.py
+++ astroid/tests/unittest_brain.py
@@ -1785,6 +1785,9 @@ class TestFunctoolsPartial:
             assert inferred.value == expected_value
 
 
+@pytest.mark.skipunless(
+    sys.version_info[:2] > (3, 4), reason="Not a problem on Python 3.4"
+)
 def test_http_client_brain():
     node = astroid.extract_node(
         """
