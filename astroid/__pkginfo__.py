9b6445dc9732d59c1ce3cce1970fa7ebfdae2f73
Claudiu Popa <pcmanticore@gmail.com>
2019-02-27 10:01:48 +0100

    Prepare 2.2.0

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -21,7 +21,7 @@ distname = "astroid"
 
 modname = "astroid"
 
-version = "2.2.0-dev1"
+version = "2.2.0"
 numversion = tuple(int(elem) for elem in version.split(".") if elem.isdigit())
 
 extras_require = {}
