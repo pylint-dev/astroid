e22858bd33f7926aff81d83df1ffb388e5cfd723
Claudiu Popa <pcmanticore@gmail.com>
2019-09-24 09:24:20 +0200

    Prepare 2.3.0

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -17,7 +17,7 @@
 
 """astroid packaging information"""
 
-version = "2.3.0.dev0"
+version = "2.3.0"
 numversion = tuple(int(elem) for elem in version.split(".") if elem.isdigit())
 
 extras_require = {}
