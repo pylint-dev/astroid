3c57b6e16c3d6eb3eac52d6337d5e07ab7de29e5
Claudiu Popa <pcmanticore@gmail.com>
2019-09-26 09:33:52 +0200

    Force typed_ast >= 1.4.0 for all supported Python versions

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -25,8 +25,7 @@ install_requires = [
     "lazy_object_proxy",
     "six",
     "wrapt",
-    'typed-ast<1.3.0;implementation_name== "cpython" and python_version<"3.7"',
-    'typed-ast>=1.3.0;implementation_name== "cpython" and python_version>="3.7" and python_version<"3.8"',
+    'typed-ast>=1.4.0,<1.5;implementation_name== "cpython" and python_version<"3.8"',
 ]
 
 # pylint: disable=redefined-builtin; why license is a builtin anyway?
