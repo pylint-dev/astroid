ee9f48c7c169876fad6e7e911a4ea0c459b2c232
Claudiu Popa <pcmanticore@gmail.com>
2019-10-09 09:27:08 +0200

    Make master 2.4.0

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -17,7 +17,7 @@
 
 """astroid packaging information"""
 
-version = "2.3.1"
+version = "2.4.0"
 numversion = tuple(int(elem) for elem in version.split(".") if elem.isdigit())
 
 extras_require = {}
