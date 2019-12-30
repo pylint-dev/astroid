4fc45c581523e4883937e1b7a5314e434e8f3125
hippo91 <guillaume.peillex@gmail.com>
2019-12-30 13:40:42 +0100

    Insures that numpy functions returning arrays are inferred only as array and not [array, Uninferable] because it could lead to false negatives.

--- tests/unittest_brain_numpy_core_umath.py
+++ tests/unittest_brain_numpy_core_umath.py
@@ -216,11 +216,9 @@ class NumpyBrainCoreUmathTest(unittest.TestCase):
             with self.subTest(typ=func_):
                 inferred_values = list(self._inferred_numpy_func_call(func_))
                 self.assertTrue(
-                    len(inferred_values) == 1
-                    or len(inferred_values) == 2
-                    and inferred_values[-1].pytype() is util.Uninferable,
+                    len(inferred_values) == 1,
                     msg="Too much inferred values ({}) for {:s}".format(
-                        inferred_values[-1].pytype(), func_
+                        inferred_values, func_
                     ),
                 )
                 self.assertTrue(
