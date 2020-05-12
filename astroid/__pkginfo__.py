27ebf1358546735dae188494627e026152825739
Claudiu Popa <pcmanticore@gmail.com>
2020-05-12 08:28:51 +0200

    Bump to 2.4.2 for the next version

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -21,7 +21,7 @@
 
 """astroid packaging information"""
 
-version = "2.4.1"
+version = "2.4.2"
 numversion = tuple(int(elem) for elem in version.split(".") if elem.isdigit())
 
 extras_require = {}
