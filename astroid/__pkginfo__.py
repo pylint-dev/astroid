6e4b275888d7b7859c2882cef0a6e3d488de3749
David Gilman <davidgilman1@gmail.com>
2020-12-14 21:27:51 -0500

    Remove Python 3.5 support
    pytest very recently dropped support for it, and if we can't run our
    tests on the platform we can't really support it

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -48,7 +48,6 @@ classifiers = [
     "Topic :: Software Development :: Quality Assurance",
     "Programming Language :: Python",
     "Programming Language :: Python :: 3",
-    "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
     "Programming Language :: Python :: 3.8",
