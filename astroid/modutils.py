dca633addd0466f2afd7b5014d0f4bf696edf70a
hippo91 <guillaume.peillex@gmail.com>
2020-12-28 15:50:21 +0100

    Adds copyright notice

--- astroid/modutils.py
+++ astroid/modutils.py
@@ -16,6 +16,7 @@
 # Copyright (c) 2019 Hugo van Kemenade <hugovk@users.noreply.github.com>
 # Copyright (c) 2019 markmcclain <markmcclain@users.noreply.github.com>
 # Copyright (c) 2019 BasPH <BasPH@users.noreply.github.com>
+# Copyright (c) 2020 hippo91 <guillaume.peillex@gmail.com>
 # Copyright (c) 2020 Peter Kolbus <peter.kolbus@gmail.com>
 
 # Licensed under the LGPL: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
