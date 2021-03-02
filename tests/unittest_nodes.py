83d04223346dc49834efb993fb83b1749d3348a3
René Fritze <rene.fritze@wwu.de>
2021-03-02 15:30:48 +0100

    minimize failing test case

--- tests/unittest_nodes.py
+++ tests/unittest_nodes.py
@@ -506,11 +506,13 @@ from ..cave import wine\n\n"""
         module = resources.build_file("data/module1abs/__init__.py", "data.module1abs")
         self.assertIn("sys", module.locals)
 
+    _pickle_names = ("dump",)# "dumps", "load", "loads")
+
     def test_conditional(self):
         module = resources.build_file("data/conditional_import/__init__.py")
         ctx = contextmod.InferenceContext()
 
-        for name in ("dump", "dumps", "load", "loads"):
+        for name in self._pickle_names:
             ctx.lookupname = name
             some = list(module[name].infer(ctx))
             assert Uninferable not in some, name
@@ -520,7 +522,7 @@ from ..cave import wine\n\n"""
         module = resources.build_file("data/conditional.py")
         ctx = contextmod.InferenceContext()
 
-        for name in ("dump", "dumps", "load", "loads"):
+        for name in self._pickle_names:
             ctx.lookupname = name
             some = list(module[name].infer(ctx))
             assert Uninferable not in some, name
