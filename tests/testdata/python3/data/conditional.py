a150d47dea1ab8e378304475b11a70e9bb17ba67
René Fritze <rene.fritze@wwu.de>
2021-03-02 11:07:20 +0100

    [tests] duplicate the conditional def test into a sub package

--- tests/testdata/python3/data/conditional.py
+++ tests/testdata/python3/data/conditional.py
@@ -1,13 +1,1 @@
-from functools import partial
-
-if False:
-    def some_function(arg):
-        pass
-
-    def another_one(arg):
-        pass
-else:
-    def some_function(arg):
-        return 2
-
-    another_one = partial(some_function, 1)
+from data.conditional_import import some_function, another_one
