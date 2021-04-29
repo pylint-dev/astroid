5c222316dcf13e8d0671903eda89273e1358c192
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-04-29 21:47:31 +0200

    Remove circular definition of __version__
    Remove pre-commit dependencies from test requirements
    there was a circular requirement between pylint in
    pre-commit requirements that needed astroid and astroid.

--- setup.py
+++ setup.py
@@ -1,10 +1,3 @@
-from pathlib import Path
-
 from setuptools import setup
 
-pkginfo = Path(__file__).parent / "astroid/__pkginfo__.py"
-
-with open(pkginfo, "rb") as fobj:
-    exec(compile(fobj.read(), pkginfo, "exec"), locals())  # pylint: disable=exec-used
-
-setup(version=__version__, use_scm_version=True)  # pylint: disable=undefined-variable
+setup(use_scm_version=True)
