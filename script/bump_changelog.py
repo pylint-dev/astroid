d55d72e234726abbc483486c131771b8d87fe34c
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-06-26 14:00:10 +0200

    Refactor of script/bump_changelog.py for easier tests

--- script/bump_changelog.py
+++ script/bump_changelog.py
@@ -24,10 +24,13 @@ def main() -> None:
     parser = argparse.ArgumentParser(add_help=__doc__)
     parser.add_argument("version", help="The version we want to release")
     args = parser.parse_args()
-    if "dev" not in args.version:
-        version = args.version
-        next_version = get_next_version(version)
-        run(version, next_version)
+    if "dev" in args.version:
+        return
+    with open(DEFAULT_CHANGELOG_PATH) as f:
+        content = f.read()
+    content = transform_content(content, args.version)
+    with open(DEFAULT_CHANGELOG_PATH, "w") as f:
+        f.write(content)
 
 
 def get_next_version(version: str) -> str:
@@ -41,23 +44,15 @@ def get_next_version(version: str) -> str:
     return ".".join(new_version)
 
 
-def run(version: str, next_version: str) -> None:
-    with open(DEFAULT_CHANGELOG_PATH) as f:
-        content = f.read()
-    content = transform_content(content, version, next_version)
-    with open(DEFAULT_CHANGELOG_PATH, "w") as f:
-        f.write(content)
-
-
-def transform_content(content: str, version: str, next_version: str) -> str:
-    wn_new_version = FULL_WHATS_NEW_TEXT.format(version=version)
+def transform_content(content: str, version: str) -> str:
+    next_version = get_next_version(version)
     wn_next_version = FULL_WHATS_NEW_TEXT.format(version=next_version)
     # There is only one field where the release date is TBA
     assert content.count(RELEASE_DATE_TEXT) == 1, TBA_ERROR_MSG
     # There is already a release note for the version we want to release
-    assert content.count(wn_new_version) == 1, NEW_VERSION_ERROR_MSG.format(
-        version=version
-    )
+    assert (
+        content.count(FULL_WHATS_NEW_TEXT.format(version=version)) == 1
+    ), NEW_VERSION_ERROR_MSG.format(version=version)
     # There is no release notes for the next version
     assert content.count(wn_next_version) == 0, NEXT_VERSION_ERROR_MSG.format(
         version=next_version
