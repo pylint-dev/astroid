90d2a46cf20cf48626ac42bbb9aa1d06248b10cf
hippo91 <guillaume.peillex@gmail.com>
2021-08-11 09:02:21 +0200

    Adds docstring

--- astroid/brain/brain_unittest.py
+++ astroid/brain/brain_unittest.py
@@ -5,6 +5,14 @@ from astroid.manager import AstroidManager
 
 
 def IsolatedAsyncioTestCaseImport():
+    """
+    In the unittest package, the IsolatedAsyncioTestCase class is imported lazily, i.e only
+    when the __getattr__ method of the unittest module is called with 'IsolatedAsyncioTestCase' as
+    argument. Thus the IsolatedAsyncioTestCase is not imported statically (during import time).
+    This function mocks a classical static import of the IsolatedAsyncioTestCase.
+
+    (see https://github.com/PyCQA/pylint/issues/4060)
+    """
     return parse("""
     from .async_case import IsolatedAsyncioTestCase
     """)
