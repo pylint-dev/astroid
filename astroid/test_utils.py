de06989e2337762d1568d6222639db9f60967f6c
hippo91 <guillaume.peillex@gmail.com>
2021-08-21 15:13:34 +0200

    Adds the extension_package_whitelist variable to the brainless manager in order unittest dealing with this managert to be ok

--- astroid/test_utils.py
+++ astroid/test_utils.py
@@ -82,4 +82,5 @@ def brainless_manager():
     m.astroid_cache = {}
     m._mod_file_cache = {}
     m._transform = transforms.TransformVisitor()
+    m.extension_package_whitelist = {}
     return m
