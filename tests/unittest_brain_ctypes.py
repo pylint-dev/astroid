c9302e9dee8264b238ee29b7a271a150583074f0
hippo91 <guillaume.peillex@gmail.com>
2021-08-28 16:32:36 +0200

    Update the unittest to take into account the brain_ctypes_bis definition. Adds more tests to be sure that the minimum has been modified through the brain

--- tests/unittest_brain_ctypes.py
+++ tests/unittest_brain_ctypes.py
@@ -1,39 +1,41 @@
+from astroid.nodes.node_classes import Const
 import pytest
 from astroid import extract_node
 
 
 # The parameters of the test define a mapping between the ctypes redefined types 
 # and the builtin types that the "value" member holds
-@pytest.mark.parametrize("c_type,builtin_type", [("c_bool", 'bool'), 
-                                                 ("c_wchar", 'str'),
-                                                 pytest.param("c_buffer", 'bytes', marks=pytest.mark.xfail(reason="c_buffer is Uninferable but for now we do not know why")),  
-                                                 ("c_char", 'bytes'),
-                                                 ("c_double", 'float'),
-                                                 ("c_float", 'float'),
-                                                 ("c_longdouble", 'float'),
-                                                 ("c_byte", 'int'),
-                                                 ("c_int", 'int'),
-                                                 ("c_int16", 'int'),
-                                                 ("c_int32", 'int'),
-                                                 ("c_int64", 'int'),
-                                                 ("c_int8", 'int'),
-                                                 ("c_long", 'int'),
-                                                 ("c_longlong", 'int'),
-                                                 ("c_short", 'int'),
-                                                 ("c_size_t", 'int'),
-                                                 ("c_ssize_t", 'int'),
-                                                 ("c_ubyte", 'int'),
-                                                 ("c_uint", 'int'),
-                                                 ("c_uint16", 'int'),
-                                                 ("c_uint32", 'int'),
-                                                 ("c_uint64", 'int'),
-                                                 ("c_uint8", 'int'),
-                                                 ("c_ulong", 'int'),
-                                                 ("c_ulonglong", 'int'),
-                                                 ("c_ushort", 'int')])
-def test_ctypes_redefined_types(c_type, builtin_type):
+@pytest.mark.parametrize("c_type,builtin_type,type_code", [("c_bool", 'bool', "?"),
+                                                           ("c_byte", 'int', "b"),
+                                                           ("c_char", 'bytes', "c"),
+                                                           ("c_double", 'float', "d"),
+                                                           pytest.param("c_buffer", 'bytes', "<class 'ctypes.c_char'>", marks=pytest.mark.xfail(reason="c_buffer is Uninferable but for now we do not know why")),  
+                                                           ("c_float", 'float', "f"),
+                                                           ("c_int", 'int', "i"),
+                                                           ("c_int16", 'int', "h"),
+                                                           ("c_int32", 'int', "i"),
+                                                           ("c_int64", 'int', "l"),
+                                                           ("c_int8", 'int', "b"),
+                                                           ("c_long", 'int', "l"),
+                                                           ("c_longdouble", 'float', "g"),
+                                                           ("c_longlong", 'int', "l"),
+                                                           ("c_short", 'int', "h"),
+                                                           ("c_size_t", 'int', "L"),
+                                                           ("c_ssize_t", 'int', "l"),
+                                                           ("c_ubyte", 'int', "B"),
+                                                           ("c_uint", 'int', "I"),
+                                                           ("c_uint16", 'int', "H"),
+                                                           ("c_uint32", 'int', "I"),
+                                                           ("c_uint64", 'int', "L"),
+                                                           ("c_uint8", 'int', "B"),
+                                                           ("c_ulong", 'int', "L"),
+                                                           ("c_ulonglong", 'int', "L"),
+                                                           ("c_ushort", 'int', "H"),
+                                                           ("c_wchar", 'str', "u")
+                                                           ])
+def test_ctypes_redefined_types_members(c_type, builtin_type, type_code):
     """
-    Test that the value member of each ctypes redefined types is correct
+    Test that the "value" and "_type_" member of each redefined types are correct
     """
     src = f"""
     import ctypes
@@ -41,10 +43,45 @@ def test_ctypes_redefined_types(c_type, builtin_type):
     x.value
     """
     node=extract_node(src)
-    ctypes_inst = next(node.expr.infer())
-    val_class = ctypes_inst.getattr('value')[0]
-    assert val_class.display_type() == "Class"
-    assert val_class.root().name == '_ctypes' 
-
     node_inf = node.inferred()[0]
     assert node_inf.pytype() == f'builtins.{builtin_type}'
+
+    src = f"""
+    import ctypes
+    x=ctypes.{c_type}("toto")
+    x._type_
+    """
+    node=extract_node(src)
+    node_inf = node.inferred()[0]
+    assert isinstance(node_inf, Const)
+    assert node_inf.value == type_code
+
+
+def test_cdata_member_access():
+    """
+    Test that the base members are still accessible. Each redefined ctypes type inherits from _SimpleCData which itself
+    inherits from _CData. Checks that _CData members are accessibles
+    """
+    src = """
+    import ctypes
+    x=ctypes.c_float(1.0)
+    x._objects
+    """
+    node=extract_node(src)
+    node_inf = node.inferred()[0]
+    assert node_inf.display_type() == 'Class'
+    assert node_inf.qname() == "_ctypes._SimpleCData._objects"
+
+
+def test_other_ctypes_member_untouched():
+    """
+    Test that other ctypes members, which are not touched by the brain, are correctly inferred
+    """
+    src = """
+    import ctypes
+    ctypes.ARRAY(3, 2)
+    """
+    node=extract_node(src)
+    node_inf = node.inferred()[0]
+    assert isinstance(node_inf, Const)
+    assert node_inf.value == 6
