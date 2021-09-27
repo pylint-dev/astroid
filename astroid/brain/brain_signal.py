96bf70129722766ce05c01dc088e098c280c97fe
Hayden Richards <supimdos@gmail.com>
2021-09-27 12:52:56 +0800

    Moved sys.platform checks outside of generated code.

--- astroid/brain/brain_signal.py
+++ astroid/brain/brain_signal.py
@@ -10,8 +10,14 @@ Without these hooks, the following are erroneously triggered by Pylint:
     * E1101: Module 'signal' has no 'Signals' member (no-member)
     * E1101: Module 'signal' has no 'Handlers' member (no-member)
     * E1101: Module 'signal' has no 'Sigmasks' member (no-member)
+
+Note that the enum.auto() values defined for the Signals, Handlers and Sigmasks
+IntEnums are just dummy integer values, and do not correspond to the actual
+standard signal numbers - which may vary depending on the system.
 """
 
+
+import sys
 
 from astroid.brain.helpers import register_module_extender
 from astroid.builder import parse
@@ -25,55 +31,64 @@ def _signals_enums_transform():
 
 def _signals_enum():
     """Generates the source code for the Signals int enum."""
-    return """
+    signals_enum = """
+    import enum
     class Signals(enum.IntEnum):
-        SIGABRT = enum.auto()
-        SIGEMT  = enum.auto()
-        SIGFPE  = enum.auto()
-        SIGILL  = enum.auto()
-        SIGINFO = enum.auto()
-        SIGINT  = enum.auto()
-        SIGSEGV = enum.auto()
-        SIGTERM = enum.auto()
-        if sys.platform != "win32":
-            SIGALRM   = enum.auto()
-            SIGBUS    = enum.auto()
-            SIGCHLD   = enum.auto()
-            SIGCONT   = enum.auto()
-            SIGHUP    = enum.auto()
-            SIGIO     = enum.auto()
-            SIGIOT    = enum.auto()
-            SIGKILL   = enum.auto()
-            SIGPIPE   = enum.auto()
-            SIGPROF   = enum.auto()
-            SIGQUIT   = enum.auto()
-            SIGSTOP   = enum.auto()
-            SIGSYS    = enum.auto()
-            SIGTRAP   = enum.auto()
-            SIGTSTP   = enum.auto()
-            SIGTTIN   = enum.auto()
-            SIGTTOU   = enum.auto()
-            SIGURG    = enum.auto()
-            SIGUSR1   = enum.auto()
-            SIGUSR2   = enum.auto()
-            SIGVTALRM = enum.auto()
-            SIGWINCH  = enum.auto()
-            SIGXCPU   = enum.auto()
-            SIGXFSZ   = enum.auto()
-        if sys.platform == "win32":
-            SIGBREAK  = enum.auto()
-        if sys.platform != "darwin" and sys.platform != "win32":
-            SIGCLD    = enum.auto()
-            SIGPOLL   = enum.auto()
-            SIGPWR    = enum.auto()
-            SIGRTMAX  = enum.auto()
-            SIGRTMIN  = enum.auto()
+        SIGABRT   = enum.auto()
+        SIGEMT    = enum.auto()
+        SIGFPE    = enum.auto()
+        SIGILL    = enum.auto()
+        SIGINFO   = enum.auto()
+        SIGINT    = enum.auto()
+        SIGSEGV   = enum.auto()
+        SIGTERM   = enum.auto()
     """
+    if sys.platform != "win32":
+        signals_enum += """
+        SIGALRM   = enum.auto()
+        SIGBUS    = enum.auto()
+        SIGCHLD   = enum.auto()
+        SIGCONT   = enum.auto()
+        SIGHUP    = enum.auto()
+        SIGIO     = enum.auto()
+        SIGIOT    = enum.auto()
+        SIGKILL   = enum.auto()
+        SIGPIPE   = enum.auto()
+        SIGPROF   = enum.auto()
+        SIGQUIT   = enum.auto()
+        SIGSTOP   = enum.auto()
+        SIGSYS    = enum.auto()
+        SIGTRAP   = enum.auto()
+        SIGTSTP   = enum.auto()
+        SIGTTIN   = enum.auto()
+        SIGTTOU   = enum.auto()
+        SIGURG    = enum.auto()
+        SIGUSR1   = enum.auto()
+        SIGUSR2   = enum.auto()
+        SIGVTALRM = enum.auto()
+        SIGWINCH  = enum.auto()
+        SIGXCPU   = enum.auto()
+        SIGXFSZ   = enum.auto()
+        """
+    if sys.platform == "win32":
+        signals_enum += """
+        SIGBREAK  = enum.auto()
+        """
+    if sys.platform != "darwin" and sys.platform != "win32":
+        signals_enum += """
+        SIGCLD    = enum.auto()
+        SIGPOLL   = enum.auto()
+        SIGPWR    = enum.auto()
+        SIGRTMAX  = enum.auto()
+        SIGRTMIN  = enum.auto()
+        """
+    return signals_enum
 
 
 def _handlers_enum():
     """Generates the source code for the Handlers int enum."""
     return """
+    import enum
     class Handlers(enum.IntEnum):
         SIG_DFL = enum.auto()
         SIG_IGN = eunm.auto()
@@ -82,13 +97,15 @@ def _handlers_enum():
 
 def _sigmasks_enum():
     """Generates the source code for the Sigmasks int enum."""
-    return """
     if sys.platform != "win32":
-        class Sigmasks(enum.IntEnum):
-            SIG_BLOCK   = enum.auto()
-            SIG_UNBLOCK = enum.auto()
-            SIG_SETMASK = enum.auto()
-    """
+        return """
+    import enum
+    class Sigmasks(enum.IntEnum):
+        SIG_BLOCK   = enum.auto()
+        SIG_UNBLOCK = enum.auto()
+        SIG_SETMASK = enum.auto()
+        """
+    return ""
 
 
 register_module_extender(AstroidManager(), "signal", _signals_enums_transform)
