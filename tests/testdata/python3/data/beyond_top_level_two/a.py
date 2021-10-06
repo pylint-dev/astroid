pylint-dev/astroid

commit 1437e73ce4bc386df76806da461f4dfc7388a0c3

Fix ``relative-beyond-top-level`` false positive (#1186)
* Fix ``relative-beyond-top-level`` false positive

astroid/nodes/scoped_nodes.py
@@ -43,6 +43,7 @@ Lambda, GeneratorExp, DictComp and SetComp to some extent).
 import builtins
 import io
 import itertools
+import os
 import typing
 from typing import List, Optional, TypeVar
 
@@ -732,10 +733,17 @@ class Module(LocalsDictNodeNG):
         if level:
             if self.package:
                 level = level - 1
+                package_name = self.name.rsplit(".", level)[0]
+            elif not os.path.exists("__init__.py") and os.path.exists(
+                modname.split(".")[0]
+            ):
+                level = level - 1
+                package_name = ""
+            else:
+                package_name = self.name.rsplit(".", level)[0]
             if level and self.name.count(".") < level:
                 raise TooManyLevelsError(level=level, name=self.name)
 
-            package_name = self.name.rsplit(".", level)[0]
         elif self.package:
             package_name = self.name
         else:
@@ -744,7 +752,7 @@ class Module(LocalsDictNodeNG):
         if package_name:
             if not modname:
                 return package_name
-            return f"{package_name}.{modname}"
+            return f"{package_name}.{modname.split('.')[0]}"
         return modname
 
     def wildcard_import_names(self):

tests/unittest_inference.py
@@ -6535,5 +6535,10 @@ b
     assert next(node.infer()).pytype() == ".B"
 
 
+def test_namespace_package() -> None:
+    """check that a file using namespace packages and relative imports is parseable"""
+    resources.build_file("data/beyond_top_level/import_package.py")
+
+
 if __name__ == "__main__":
     unittest.main()

tests/testdata/python3/data/beyond_top_level/import_package.py
@@ -1,0 +1,3 @@
+from namespace_package import top_level_function
+
+top_level_function.do_something()

tests/testdata/python3/data/beyond_top_level/namespace_package/lower_level/helper_function.py
@@ -1,0 +1,5 @@
+from ..plugin_api import top_message
+
+
+def plugin_message(msg):
+    return "plugin_message: %s" % top_message(msg)

tests/testdata/python3/data/beyond_top_level/namespace_package/plugin_api.py
@@ -1,0 +1,2 @@
+def top_message(msg):
+    return "top_message: %s" % msg

tests/testdata/python3/data/beyond_top_level/namespace_package/top_level_function.py
@@ -1,0 +1,5 @@
+from .lower_level.helper_function import plugin_message
+
+
+def do_something():
+    return plugin_message("called by do_something")

commit ccfc156b60b019360d8a614754da7a17e470b799

Fix regression introduced by #1186 and add tests This closes #1200

astroid/nodes/scoped_nodes.py
@@ -752,7 +752,7 @@ class Module(LocalsDictNodeNG):
         if package_name:
             if not modname:
                 return package_name
-            return f"{package_name}.{modname.split('.')[0]}"
+            return f"{package_name}.{modname}"
         return modname
 
     def wildcard_import_names(self):

tests/testdata/python3/data/beyond_top_level_two/a.py
@@ -1,0 +1,7 @@
+# pylint: disable=missing-docstring
+
+from .level1.beyond_top_level_two import func
+
+
+def do_something(var, some_other_var):  # error
+    func(var, some_other_var)

tests/testdata/python3/data/beyond_top_level_two/level1/__init__.py
@@ -1,0 +1,2 @@
+def func(var):
+    pass

tests/testdata/python3/data/beyond_top_level_two/level1/beyond_top_level_two.py
@@ -1,0 +1,2 @@
+def func(var, some_other_var):
+    pass

tests/unittest_inference.py
@@ -6540,5 +6540,11 @@ def test_namespace_package() -> None:
     resources.build_file("data/beyond_top_level/import_package.py")
 
 
+def test_namespace_package_same_name() -> None:
+    """check that a file using namespace packages and relative imports
+    with similar names is parseable"""
+    resources.build_file("data/beyond_top_level_two/a.py")
+
+
 if __name__ == "__main__":
     unittest.main()