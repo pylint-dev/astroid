4c3b3b316e280a04d0a5ca7c481430be3b8d7a00
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-10-25 22:02:34 +0200

    Bump astroid to 2.8.4, update changelog

--- astroid/nodes/scoped_nodes.py
+++ astroid/nodes/scoped_nodes.py
@@ -24,8 +24,8 @@
 # Copyright (c) 2020 Tim Martin <tim@asymptotic.co.uk>
 # Copyright (c) 2020 Ram Rachum <ram@rachum.com>
 # Copyright (c) 2021 Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
-# Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
 # Copyright (c) 2021 Pierre Sassoulas <pierre.sassoulas@gmail.com>
+# Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
 # Copyright (c) 2021 David Liu <david@cs.toronto.edu>
 # Copyright (c) 2021 pre-commit-ci[bot] <bot@noreply.github.com>
 # Copyright (c) 2021 doranid <ddandd@gmail.com>
