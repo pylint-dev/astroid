2b6e7d0f296e4eb01068df6360443ac2da827b48
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-06 21:17:26 +0200

    Add disable

--- astroid/exceptions.py
+++ astroid/exceptions.py
@@ -282,6 +282,9 @@ class StatementMissing(ParentMissingError):
     """
 
     def __init__(self, target: "nodes.NodeNG") -> None:
+        # See: https://github.com/PyCQA/pylint/issues/2903
+        # and: https://github.com/PyCQA/astroid/pull/1217#discussion_r744149027
+        # pylint: disable-next=bad-super-call
         super(ParentMissingError, self).__init__(
             message=f"Statement not found on {target!r}"
         )
