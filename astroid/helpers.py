e1ecb6d3d5a6555a807ab3db0f2f938d8d73e95c
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-08 13:25:14 +0200

    Fix crash on inference of __len__ (#1234)
    Co-authored-by: Marc Mueller <30130371+cdce8p@users.noreply.github.com>

--- astroid/helpers.py
+++ astroid/helpers.py
@@ -258,7 +258,7 @@ def object_len(node, context=None):
     if (
         isinstance(node_frame, scoped_nodes.FunctionDef)
         and node_frame.name == "__len__"
-        and inferred_node is not None
+        and hasattr(inferred_node, "_proxied")
         and inferred_node._proxied == node_frame.parent
     ):
         message = (
--- tests/unittest_brain.py
+++ tests/unittest_brain.py
@@ -3112,6 +3112,8 @@ def test_str_and_bytes(code, expected_class, expected_value):
 def test_no_recursionerror_on_self_referential_length_check() -> None:
     """
     Regression test for https://github.com/PyCQA/astroid/issues/777
+
+    This test should only raise an InferenceError and no RecursionError.
     """
     with pytest.raises(InferenceError):
         node = astroid.extract_node(
@@ -3120,6 +3122,57 @@ def test_no_recursionerror_on_self_referential_length_check() -> None:
             def __len__(self) -> int:
                 return len(self)
         len(Crash()) #@
+        """
+        )
+        assert isinstance(node, nodes.NodeNG)
+        node.inferred()
+
+
+def test_inference_on_outer_referential_length_check() -> None:
+    """
+    Regression test for https://github.com/PyCQA/pylint/issues/5244
+    See also https://github.com/PyCQA/astroid/pull/1234
+
+    This test should succeed without any error.
+    """
+    node = astroid.extract_node(
+        """
+    class A:
+        def __len__(self) -> int:
+            return 42
+
+    class Crash:
+        def __len__(self) -> int:
+            a = A()
+            return len(a)
+
+    len(Crash()) #@
+    """
+    )
+    inferred = node.inferred()
+    assert len(inferred) == 1
+    assert isinstance(inferred[0], nodes.Const)
+    assert inferred[0].value == 42
+
+
+def test_no_attributeerror_on_self_referential_length_check() -> None:
+    """
+    Regression test for https://github.com/PyCQA/pylint/issues/5244
+    See also https://github.com/PyCQA/astroid/pull/1234
+
+    This test should only raise an InferenceError and no AttributeError.
+    """
+    with pytest.raises(InferenceError):
+        node = astroid.extract_node(
+            """
+        class MyClass:
+            def some_func(self):
+                return lambda: 42
+
+            def __len__(self):
+                return len(self.some_func())
+
+        len(MyClass()) #@
         """
         )
         assert isinstance(node, nodes.NodeNG)
