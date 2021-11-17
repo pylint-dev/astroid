4bbee1c81d11ec1479e1fe361da34eddab5f0792
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-18 00:10:47 +0100

    Change ``NoReturn`` to be encapsulated in strings

--- astroid/nodes/node_ng.py
+++ astroid/nodes/node_ng.py
@@ -32,10 +32,11 @@ from astroid.nodes.const import OP_PRECEDENCE
 if TYPE_CHECKING:
     from astroid import nodes
 
-if sys.version_info >= (3, 6, 2):
-    from typing import NoReturn
-else:
-    from typing_extensions import NoReturn
+    if sys.version_info >= (3, 6, 2):
+        # To be fixed with https://github.com/PyCQA/pylint/pull/5316
+        from typing import NoReturn  # pylint: disable=unused-import
+    else:
+        from typing_extensions import NoReturn
 
 if sys.version_info >= (3, 8):
     from typing import Literal
@@ -276,7 +277,7 @@ class NodeNG:
 
     def statement(
         self, *, future: Literal[None, True] = None
-    ) -> Union["nodes.Statement", "nodes.Module", NoReturn]:
+    ) -> Union["nodes.Statement", "nodes.Module", "NoReturn"]:
         """The first parent node, including self, marked as statement node.
 
         TODO: Deprecate the future parameter and only raise StatementMissing and return
