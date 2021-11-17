f5efd6083647371ba488f5fd7c7a638bbb408e78
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2021-11-17 11:13:09 +0100

    Add missing type

--- astroid/builder.py
+++ astroid/builder.py
@@ -263,7 +263,7 @@ class AstroidBuilder(raw_building.InspectBuilder):
             pass
 
 
-def build_namespace_package_module(name, path: List[str]) -> nodes.Module:
+def build_namespace_package_module(name: str, path: List[str]) -> nodes.Module:
     return nodes.Module(name, doc="", path=path, package=True)
 
 
