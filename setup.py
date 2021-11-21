fc651fe167a705737cdbdf8ac683ab8f3caf342e
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2021-11-21 16:28:27 +0100

    Add an exception at install for python < 3.6.2

--- setup.py
+++ setup.py
@@ -1,3 +1,19 @@
+import sys
+
 from setuptools import setup
 
+
+class AstroidIncompatiblePythonError(Exception):
+    def __init__(self) -> None:
+        super().__init__(
+            "The last version compatible with python '3.6.0' and '3.6.1' is astroid '2.6.6'"
+            f"You're using {'.'.join([str(v) for v in sys.version_info[:3]])}. "
+            "Please install astroid 2.6.6 explicitly or upgrade your python interpreter "
+            "to at least 3.6.2. Remember that Python 3.6 end life is December 2021. "
+            "See https://github.com/PyCQA/pylint/issues/5065 for more detail."
+        )
+
+
+if sys.version_info < (3, 6, 2):
+    raise AstroidIncompatiblePythonError()
 setup()
