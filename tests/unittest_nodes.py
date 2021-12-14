e5cdda559be31700548e1b8e715d62c5e8e9d5f4
kasium <15907922+kasium@users.noreply.github.com>
2021-12-14 20:15:19 +0100

    Update tests/unittest_nodes.py
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>

--- tests/unittest_nodes.py
+++ tests/unittest_nodes.py
@@ -1584,6 +1584,7 @@ def test_get_doc() -> None:
     )
     assert node.doc == "Docstring"
     assert isinstance(node.doc_node, nodes.Const)
+    assert node.doc_node.value == "Docstring"
     assert node.doc_node.lineno == 3
     assert node.doc_node.col_offset == 4
     if PY38_PLUS:
