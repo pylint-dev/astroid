67281cb9e29ca0e4168ceb333a17287358008bf2
Tushar Sadhwani <tushar@deepsource.io>
2021-12-20 14:12:14 +0530

    Hack to trigger ci

--- astroid/nodes/as_string.py
+++ astroid/nodes/as_string.py
@@ -22,7 +22,7 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 
 """This module renders Astroid nodes as string"""
-from typing import TYPE_CHECKING, List
+from typing import TYPE_CHECKING, List, Union
 
 if TYPE_CHECKING:
     from astroid.nodes.node_classes import (
