31199971e906b2094048f632158bf65211fa6364
Jacob Bogdanov <jacob@bogdanov.dev>
2022-01-18 17:28:23 -0500

    Update tests/unittest_brain.py
    Co-authored-by: Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>

--- tests/unittest_brain.py
+++ tests/unittest_brain.py
@@ -2212,6 +2212,11 @@ class AttrsTest(unittest.TestCase):
             self.assertIsInstance(should_be_unknown, astroid.Unknown)
 
     def test_attrs_transform(self) -> None:
+        """Test brain for decorators of the 'attrs' package.
+        
+        Package added support for 'attrs' a long side 'attr' in v21.3.0.
+        See: https://github.com/python-attrs/attrs/releases/tag/21.3.0
+        """
         module = astroid.parse(
             """
         import attrs
