552f1c19aab0719e7b30bb02e032d4bfe655f343
Jacob Walls <jacobtylerwalls@gmail.com>
2022-02-09 20:10:08 -0500

    Improve order of keyword-only args in argnames() (#1387)

--- astroid/nodes/scoped_nodes/scoped_nodes.py
+++ astroid/nodes/scoped_nodes/scoped_nodes.py
@@ -1369,7 +1369,7 @@ class Lambda(mixins.FilterStmtsMixin, LocalsDictNodeNG):
     def argnames(self) -> List[str]:
         """Get the names of each of the arguments, including that
         of the collections of variable-length arguments ("args", "kwargs",
-        etc.), as well as keyword-only arguments.
+        etc.), as well as positional-only and keyword-only arguments.
 
         :returns: The names of the arguments.
         :rtype: list(str)
@@ -1378,9 +1378,9 @@ class Lambda(mixins.FilterStmtsMixin, LocalsDictNodeNG):
             names = _rec_get_names(self.args.arguments)
         else:
             names = []
-        names += [elt.name for elt in self.args.kwonlyargs]
         if self.args.vararg:
             names.append(self.args.vararg)
+        names += [elt.name for elt in self.args.kwonlyargs]
         if self.args.kwarg:
             names.append(self.args.kwarg)
         return names
