85bb56e8253d169776bcee9d1b5a5bebfba7ecc2
Mark Byrne <mbyrnepr2@gmail.com>
2022-03-09 20:22:05 +0100

    Use Daniël's improvements:
    - Fixup imports
    - More accurate unittest naming
    - Use typehints

--- tests/unittest_brain_numpy_core_numeric.py
+++ tests/unittest_brain_numpy_core_numeric.py
@@ -4,11 +4,12 @@
 # Copyright (c) 2021 Pierre Sassoulas <pierre.sassoulas@gmail.com>
 # Copyright (c) 2021 Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
 # Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
-
-import unittest
-
 # Licensed under the LGPL: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
+
+import unittest
+from typing import List
+
 import pytest
 
 try:
@@ -74,7 +75,7 @@ class BrainNumpyCoreNumericTest(unittest.TestCase):
         ("ones", ["shape", "dtype", "order"]),
     ],
 )
-def test_parameters(method, expected_args) -> None:
+def test_function_parameters(method: str, expected_args: List[str]) -> None:
     instance = builder.extract_node(
         f"""
     import numpy
