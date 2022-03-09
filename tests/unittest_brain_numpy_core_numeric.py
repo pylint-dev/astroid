c3504e0c32173f3598042c0efc8df061d4b0ebf4
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-03-09 20:32:45 +0100

    Update tests/unittest_brain_numpy_core_numeric.py

--- tests/unittest_brain_numpy_core_numeric.py
+++ tests/unittest_brain_numpy_core_numeric.py
@@ -4,6 +4,7 @@
 # Copyright (c) 2021 Pierre Sassoulas <pierre.sassoulas@gmail.com>
 # Copyright (c) 2021 Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
 # Copyright (c) 2021 Marc Mueller <30130371+cdce8p@users.noreply.github.com>
+
 # Licensed under the LGPL: https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 
