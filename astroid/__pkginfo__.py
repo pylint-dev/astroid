36b81ead6dcecde342c696eab3089267acbe4ff3
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2022-04-19 21:08:40 +0200

    Bump astroid to 2.11.3, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "2.11.2"
+__version__ = "2.11.3"
 version = __version__
