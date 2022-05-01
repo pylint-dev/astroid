f53159ea09fc7c7106f4ca321d2ddeaf66938693
Jacob Walls <jacobtylerwalls@gmail.com>
2022-05-01 12:36:58 -0400

    Ensure parent packages are not imported

--- astroid/interpreter/_import/util.py
+++ astroid/interpreter/_import/util.py
@@ -2,15 +2,34 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-from importlib.util import find_spec
+from importlib.util import _find_spec_from_path
 
 
-def is_namespace(modname):
-    try:
-        found_spec = find_spec(modname)
-    except ValueError:
-        # execution of .pth files may not have __spec__
-        return True
+def is_namespace(modname: str) -> bool:
+    found_spec = None
+
+    # find_spec() attempts to import parent packages when given dotted paths.
+    # That's unacceptable here, so we fallback to _find_spec_from_path(), which does
+    # not, but requires instead that each single parent ('astroid', 'nodes', etc.)
+    # be specced from left to right.
+    working_modname = ""
+    last_parent = None
+    for component in modname.split("."):
+        if working_modname:
+            working_modname += "." + component
+        else:
+            # First component
+            working_modname = component
+        try:
+            found_spec = _find_spec_from_path(working_modname, last_parent)
+        except (
+            AttributeError,  # TODO: remove AttributeError when 3.7+ is min
+            ValueError,
+        ):
+            # executed .pth files may not have __spec__
+            return True
+        last_parent = working_modname
+
     if found_spec is None:
         return
     # origin can be either a string on older Python versions
