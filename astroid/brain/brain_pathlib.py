55778fb52b7b56385980ca407e60f31824bcf6f5
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-06-02 10:32:23 +0200

    Fix typing and inference of index look up

--- astroid/brain/brain_pathlib.py
+++ astroid/brain/brain_pathlib.py
@@ -2,36 +2,50 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-from typing import Iterator, Optional
+from __future__ import annotations
 
-from astroid import bases, context, inference_tip, nodes
+from collections.abc import Iterator
+
+from astroid import bases, context, extract_node, inference_tip, nodes
 from astroid.exceptions import InferenceError, UseInferenceDefault
 from astroid.manager import AstroidManager
 
+PATH = extract_node(
+    """from pathlib import Path
+Path
+"""
+)
+PATH_CLASSDEF: nodes.ClassDef = next(PATH.infer())
+
 
 def _looks_like_parents_subscript(node: nodes.Subscript) -> bool:
-    return isinstance(node.value, nodes.Attribute) and node.value.attrname == "parents"
+    if not isinstance(node.value, nodes.Name) and not (
+        isinstance(node.value, nodes.Attribute) and node.value.attrname == "parents"
+    ):
+        return False
 
-
-def infer_parents_subscript(
-    subscript_node: nodes.Subscript, ctx: Optional[context.InferenceContext] = None
-) -> Iterator[bases.Instance]:
     try:
-        value = next(subscript_node.value.infer())
-    except (InferenceError, StopIteration) as exc:
-        raise UseInferenceDefault from exc
+        value = next(node.value.infer())
+    except (InferenceError, StopIteration):
+        return False
 
     if not (
         isinstance(value, bases.Instance)
         and isinstance(value._proxied, nodes.ClassDef)
         and value.qname() == "pathlib._PathParents"
     ):
-        raise UseInferenceDefault
+        return False
 
-    if isinstance(subscript_node.slice, nodes.Slice):
-        raise UseInferenceDefault
+    return True
 
-    yield from subscript_node.value.expr.infer(context=ctx)
+
+def infer_parents_subscript(
+    subscript_node: nodes.Subscript, ctx: context.InferenceContext | None = None
+) -> Iterator[bases.Instance]:
+    if isinstance(subscript_node.slice, nodes.Const):
+        return iter((PATH_CLASSDEF.instantiate_class(),))
+
+    raise UseInferenceDefault
 
 
 AstroidManager().register_transform(
--- tests/unittest_brain_pathlib.py
+++ tests/unittest_brain_pathlib.py
@@ -2,7 +2,6 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-import pytest
 
 import astroid
 from astroid import bases
@@ -29,24 +28,27 @@ def test_inference_parents() -> None:
 
 def test_inference_parents_subscript_index() -> None:
     """Test inference of ``pathlib.Path.parents``, accessed by index."""
-    name_node = astroid.extract_node(
+    parents, path = astroid.extract_node(
         """
     from pathlib import Path
 
     current_path = Path().resolve()
-    parent_path = current_path.parents[2]
-    parent_path
+    path_parents = current_path.parents
+    path_parents  #@
+    path_parents[2]  #@
     """
     )
-    inferred = name_node.inferred()
+    inferred = parents.inferred()
+    assert len(inferred) == 1
+    assert isinstance(inferred[0], bases.Instance)
+    assert inferred[0].qname() == "pathlib._PathParents"
+
+    inferred = path.inferred()
     assert len(inferred) == 1
     assert isinstance(inferred[0], bases.Instance)
     assert inferred[0].qname() == "pathlib.Path"
 
 
-@pytest.mark.skipif(
-    not PY310_PLUS, reason="The parents sequence supports slices since Python 3.10."
-)
 def test_inference_parents_subscript_slice() -> None:
     """Test inference of ``pathlib.Path.parents``, accessed by slice."""
     name_node = astroid.extract_node(
@@ -60,8 +62,11 @@ def test_inference_parents_subscript_slice() -> None:
     )
     inferred = name_node.inferred()
     assert len(inferred) == 1
-    assert isinstance(inferred[0], bases.Instance)
-    assert inferred[0].qname() == "builtins.tuple"
+    if PY310_PLUS:
+        assert isinstance(inferred[0], bases.Instance)
+        assert inferred[0].qname() == "builtins.tuple"
+    else:
+        assert inferred[0] is Uninferable
 
 
 def test_inference_parents_subscript_not_path() -> None:
@@ -72,7 +77,7 @@ def test_inference_parents_subscript_not_path() -> None:
         parents = 42
 
     c = A()
-    error = c.parents[2]
+    error = c.parents[:2]
     error
     """
     )
