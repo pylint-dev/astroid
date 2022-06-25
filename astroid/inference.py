7460c3a14bb713f88aa56c314bc8bee291f3458f
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-06-25 23:05:49 +0200

    Add comments

--- astroid/inference.py
+++ astroid/inference.py
@@ -359,6 +359,8 @@ def infer_attribute(
     return InferenceErrorInfo(node=self, context=context)
 
 
+# The order of the decorators here is important
+# See https://github.com/PyCQA/astroid/commit/0a8a75db30da060a24922e05048bc270230f5
 nodes.Attribute._infer = decorators.raise_if_nothing_inferred(  # type: ignore[assignment]
     decorators.path_wrapper(infer_attribute)
 )
@@ -445,6 +447,8 @@ def infer_subscript(
     return None
 
 
+# The order of the decorators here is important
+# See https://github.com/PyCQA/astroid/commit/0a8a75db30da060a24922e05048bc270230f5
 nodes.Subscript._infer = decorators.raise_if_nothing_inferred(  # type: ignore[assignment]
     decorators.path_wrapper(infer_subscript)
 )
