69c5175e05fb4c36f8fe429aba1a60fc892c87e7
Jacob Walls <jacobtylerwalls@gmail.com>
2022-08-29 02:38:51 -0400

    Prevent first-party imports from being resolved to `site-packages` (#1756)
    Co-authored-by: Pierre Sassoulas <pierre.sassoulas@gmail.com>

--- tests/unittest_manager.py
+++ tests/unittest_manager.py
@@ -17,7 +17,7 @@ from astroid import manager, test_utils
 from astroid.const import IS_JYTHON, IS_PYPY
 from astroid.exceptions import AstroidBuildingError, AstroidImportError
 from astroid.interpreter._import import util
-from astroid.modutils import is_standard_module
+from astroid.modutils import EXT_LIB_DIRS, is_standard_module
 from astroid.nodes import Const
 from astroid.nodes.scoped_nodes import ClassDef
 
@@ -130,6 +130,9 @@ class AstroidManagerTest(
     def test_module_is_not_namespace(self) -> None:
         self.assertFalse(util.is_namespace("tests.testdata.python3.data.all"))
         self.assertFalse(util.is_namespace("__main__"))
+        self.assertFalse(
+            util.is_namespace(list(EXT_LIB_DIRS)[0].rsplit("/", maxsplit=1)[-1]),
+        )
         self.assertFalse(util.is_namespace("importlib._bootstrap"))
 
     def test_module_unexpectedly_missing_spec(self) -> None:
