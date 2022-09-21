63e8bae2b15e64ee67a33e1e50743b6c5e8a0859
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2022-09-21 16:52:35 +0200

    Finish typing of ``AstroidManagerBrain``

--- astroid/typing.py
+++ astroid/typing.py
@@ -8,8 +8,9 @@ import sys
 from typing import TYPE_CHECKING, Any, Callable, Union
 
 if TYPE_CHECKING:
-    from astroid import bases, nodes, transforms, util
+    from astroid import bases, exceptions, nodes, transforms, util
     from astroid.context import InferenceContext
+    from astroid.interpreter._import import spec
 
 if sys.version_info >= (3, 8):
     from typing import TypedDict
@@ -33,11 +34,13 @@ class AstroidManagerBrain(TypedDict):
     """Dictionary to store relevant information for a AstroidManager class."""
 
     astroid_cache: dict[str, nodes.Module]
-    _mod_file_cache: dict
-    _failed_import_hooks: list
+    _mod_file_cache: dict[
+        tuple[str, str | None], spec.ModuleSpec | exceptions.AstroidImportError
+    ]
+    _failed_import_hooks: list[Callable[[str], nodes.Module]]
     always_load_extensions: bool
     optimize_ast: bool
-    extension_package_whitelist: set
+    extension_package_whitelist: set[str]
     _transform: transforms.TransformVisitor
 
 
