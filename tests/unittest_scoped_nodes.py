pylint-dev/astroid

commit 36d22051b0af46e3954f63f1b09c7a2fb1fcc2c5

Add typing to ``metaclass`` methods (#1678)
* Add typing to ``metaclass`` methods

* Change to ``NodeNG``

astroid/nodes/scoped_nodes/scoped_nodes.py
@@ -2811,7 +2811,9 @@ class ClassDef(
             return builtin_lookup("type")[1][0]
         return None
 
-    def declared_metaclass(self, context=None):
+    def declared_metaclass(
+        self, context: InferenceContext | None = None
+    ) -> NodeNG | None:
         """Return the explicit declared metaclass for the current class.
 
         An explicit declared metaclass is defined
@@ -2822,7 +2824,6 @@ class ClassDef(
 
         :returns: The metaclass of this class,
             or None if one could not be found.
-        :rtype: NodeNG or None
         """
         for base in self.bases:
             try:
@@ -2840,14 +2841,16 @@ class ClassDef(
                 return next(
                     node
                     for node in self._metaclass.infer(context=context)
-                    if node is not util.Uninferable
+                    if isinstance(node, NodeNG)
                 )
             except (InferenceError, StopIteration):
                 return None
 
         return None
 
-    def _find_metaclass(self, seen=None, context=None):
+    def _find_metaclass(
+        self, seen: set[ClassDef] | None = None, context: InferenceContext | None = None
+    ) -> NodeNG | None:
         if seen is None:
             seen = set()
         seen.add(self)
@@ -2861,7 +2864,7 @@ class ClassDef(
                         break
         return klass
 
-    def metaclass(self, context=None):
+    def metaclass(self, context: InferenceContext | None = None) -> NodeNG | None:
         """Get the metaclass of this class.
 
         If this class does not define explicitly a metaclass,
@@ -2869,7 +2872,6 @@ class ClassDef(
         instead.
 
         :returns: The metaclass of this class.
-        :rtype: NodeNG or None
         """
         return self._find_metaclass(context=context)
 

commit 8a633ad453ea4ea81ae8300f72ae9ad974488504

Fix detecting invalid metaclasses
Regression from #1678

astroid/nodes/scoped_nodes/scoped_nodes.py
@@ -2841,7 +2841,7 @@ class ClassDef(
                 return next(
                     node
                     for node in self._metaclass.infer(context=context)
-                    if isinstance(node, NodeNG)
+                    if node is not util.Uninferable
                 )
             except (InferenceError, StopIteration):
                 return None

tests/unittest_scoped_nodes.py
@@ -1415,6 +1415,20 @@ class ClassNodeTest(ModuleLoader, unittest.TestCase):
         inferred = next(klass.infer())
         self.assertIsNone(inferred.metaclass())
 
+    @staticmethod
+    def test_with_invalid_metaclass():
+        klass = extract_node(
+            """
+        class InvalidAsMetaclass: ...
+
+        class Invalid(metaclass=InvalidAsMetaclass()):  #@
+            pass
+        """
+        )
+        inferred = next(klass.infer())
+        metaclass = inferred.metaclass()
+        assert isinstance(metaclass, Instance)
+
     def test_nonregr_infer_callresult(self) -> None:
         astroid = builder.parse(
             """