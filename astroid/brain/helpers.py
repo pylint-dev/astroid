fa66ac9c4403760ebc1a547acaaabc01e7443f85
Nick Drozd <nicholasdrozd@gmail.com>
2022-11-30 12:03:20 -0500

    future annotations

--- astroid/brain/helpers.py
+++ astroid/brain/helpers.py
@@ -2,6 +2,8 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
+from __future__ import annotations
+
 from collections.abc import Callable
 
 from astroid.manager import AstroidManager
