8c681f03b79c7b8d1d9273a39584fd72d5785880
Dani Alcala <112832187+clavedeluna@users.noreply.github.com>
2022-12-30 11:39:58 -0300

    brain tip for numpy masked_invalid (#1931)
    * brain tip for numpy masked_invalid
    
    * add unit test
    
    * refactor tests and update changelog
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- tests/unittest_brain_numpy_ma.py
+++ tests/unittest_brain_numpy_ma.py
@@ -20,34 +20,29 @@ class TestBrainNumpyMa:
     Test the numpy ma brain module
     """
 
-    @staticmethod
-    def test_numpy_ma_masked_where_returns_maskedarray():
-        """
-        Test that calls to numpy ma masked_where returns a MaskedArray object.
-
-        The "masked_where" node is an Attribute
-        """
-        src = """
-        import numpy as np
-        data = np.ndarray((1,2))
-        np.ma.masked_where([1, 0, 0], data)
-        """
-        node = builder.extract_node(src)
+    def _assert_maskedarray(self, code):
+        node = builder.extract_node(code)
         cls_node = node.inferred()[0]
         assert cls_node.pytype() == "numpy.ma.core.MaskedArray"
 
-    @staticmethod
-    def test_numpy_ma_masked_where_returns_maskedarray_bis():
+    @pytest.mark.parametrize("alias_import", [True, False])
+    @pytest.mark.parametrize("ma_function", ["masked_invalid", "masked_where"])
+    def test_numpy_ma_returns_maskedarray(self, alias_import, ma_function):
         """
-        Test that calls to numpy ma masked_where returns a MaskedArray object
+        Test that calls to numpy ma functions return a MaskedArray object.
 
-        The "masked_where" node is a Name
+        The `ma_function` node is an Attribute or a Name
         """
-        src = """
-        from numpy.ma import masked_where
+        import_str = (
+            "import numpy as np"
+            if alias_import
+            else f"from numpy.ma import {ma_function}"
+        )
+        func = f"np.ma.{ma_function}" if alias_import else ma_function
+
+        src = f"""
+        {import_str}
         data = np.ndarray((1,2))
-        masked_where([1, 0, 0], data)
+        {func}([1, 0, 0], data)
         """
-        node = builder.extract_node(src)
-        cls_node = node.inferred()[0]
-        assert cls_node.pytype() == "numpy.ma.core.MaskedArray"
+        self._assert_maskedarray(src)
