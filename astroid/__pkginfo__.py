2d15e20bfce71e48b14bdc483b7c8afb8eedcaef
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-03-06 00:10:08 +0100

    Bump astroid to 2.16.0-dev0, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "2.15.0"
+__version__ = "2.16.0dev0"
 version = __version__
