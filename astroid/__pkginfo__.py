85b698466afe12c750842dc2522f2823365d9fbc
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-04-02 21:19:53 +0200

    Bump astroid to 2.15.2, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "2.15.1"
+__version__ = "2.15.2"
 version = __version__
