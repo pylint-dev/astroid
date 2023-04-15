15722e60a1180d1d766d8daa6711b87fafbc0674
Jacob Walls <jacobtylerwalls@gmail.com>
2023-04-15 10:58:37 -0400

    fixup -- allow either arguments or list

--- astroid/nodes/scoped_nodes/scoped_nodes.py
+++ astroid/nodes/scoped_nodes/scoped_nodes.py
@@ -1693,12 +1693,17 @@ class FunctionDef(_base_nodes.MultiLineBlockNode, _base_nodes.Statement, Lambda)
         if (
             self.name == "with_metaclass"
             and caller is not None
-            and isinstance(caller.args, Arguments)
-            and caller.args.args
             and len(self.args.args) == 1
             and self.args.vararg is not None
         ):
-            metaclass = next(caller.args.args[0].infer(context), None)
+            if isinstance(caller.args, Arguments):
+                metaclass = next(caller.args.args[0].infer(context), None)
+            elif isinstance(caller.args, list):
+                metaclass = next(caller.args[0].infer(context), None)
+            else:
+                raise TypeError(
+                    f"caller.args was neither Arguments nor list; got {type(caller.args)}"
+                )
             if isinstance(metaclass, ClassDef):
                 try:
                     class_bases = [
