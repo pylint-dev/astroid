49d8d2739e002a286303701bfec28df90c564027
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-04-23 23:05:25 +0200

    Code review

--- astroid/__init__.py
+++ astroid/__init__.py
@@ -187,7 +187,7 @@ if (
     and callable(getattr(tokenize, "_compile", None))
     and getattr(tokenize._compile, "__wrapped__", None) is None  # type: ignore[attr-defined]
 ):
-    tokenize._compile = functools.lru_cache()(tokenize._compile)  # type: ignore[attr-defined]
+    tokenize._compile = functools.lru_cache(tokenize._compile)  # type: ignore[attr-defined]
 
 # load brain plugins
 for module in BRAIN_MODULES_DIRECTORY.iterdir():
