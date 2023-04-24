420a59a7da81828e982fabfd34e1533ffddf6400
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2023-04-24 10:51:12 +0200

    Bump astroid to 2.15.4, update changelog (#2144)

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "2.15.3"
+__version__ = "2.15.4"
 version = __version__
