pylint-dev/astroid

commit bbcc58bd52e7f295b77a8618b19b2364625590a2

Change initialization of ``AstroidManager``

astroid/manager.py
@@ -25,6 +25,7 @@ from astroid.modutils import (
     modpath_from_file,
 )
 from astroid.transforms import TransformVisitor
+from astroid.typing import AstroidManagerBrain
 
 if TYPE_CHECKING:
     from astroid import nodes
@@ -46,20 +47,28 @@ class AstroidManager:
     """
 
     name = "astroid loader"
-    brain = {}
+    brain: AstroidManagerBrain = {
+        "astroid_cache": {},
+        "_mod_file_cache": {},
+        "_failed_import_hooks": [],
+        "always_load_extensions": False,
+        "optimize_ast": False,
+        "extension_package_whitelist": set(),
+        "_transform": TransformVisitor(),
+    }
     max_inferable_values: ClassVar[int] = 100
 
     def __init__(self):
-        self.__dict__ = AstroidManager.brain
-        if not self.__dict__:
-            # NOTE: cache entries are added by the [re]builder
-            self.astroid_cache = {}
-            self._mod_file_cache = {}
-            self._failed_import_hooks = []
-            self.always_load_extensions = False
-            self.optimize_ast = False
-            self.extension_package_whitelist = set()
-            self._transform = TransformVisitor()
+        # NOTE: cache entries are added by the [re]builder
+        self.astroid_cache = AstroidManager.brain["astroid_cache"]
+        self._mod_file_cache = AstroidManager.brain["_mod_file_cache"]
+        self._failed_import_hooks = AstroidManager.brain["_failed_import_hooks"]
+        self.always_load_extensions = AstroidManager.brain["always_load_extensions"]
+        self.optimize_ast = AstroidManager.brain["optimize_ast"]
+        self.extension_package_whitelist = AstroidManager.brain[
+            "extension_package_whitelist"
+        ]
+        self._transform = AstroidManager.brain["_transform"]
 
     @property
     def register_transform(self):

astroid/typing.py
@@ -3,10 +3,10 @@
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
 import sys
-from typing import TYPE_CHECKING, Any, Callable
+from typing import TYPE_CHECKING, Any, Callable, Dict, List, Set
 
 if TYPE_CHECKING:
-    from astroid import nodes
+    from astroid import nodes, transforms
     from astroid.context import InferenceContext
 
 if sys.version_info >= (3, 8):
@@ -25,3 +25,15 @@ class InferenceErrorInfo(TypedDict):
 
 
 InferFn = Callable[..., Any]
+
+
+class AstroidManagerBrain(TypedDict):
+    """Dictionary to store relevant information for a AstroidManager class."""
+
+    astroid_cache: Dict
+    _mod_file_cache: Dict
+    _failed_import_hooks: List
+    always_load_extensions: bool
+    optimize_ast: bool
+    extension_package_whitelist: Set
+    _transform: "transforms.TransformVisitor"

commit b08166bf452852348fdf2c0c6aceb89997094742

Fix brain dict regression (#2204)
Fix regression resulting in ignored pylint settings

We get rid of the immutable instance attributes in AstroidManager,
ensuring that these always mutate the global state instead of
instance's.
This fixes a regression introduced in commit
bbcc58bd52e7f295b77a8618b19b2364625590a2.

Fixes #2200

astroid/manager.py
@@ -69,12 +69,26 @@ class AstroidManager:
         self.astroid_cache = AstroidManager.brain["astroid_cache"]
         self._mod_file_cache = AstroidManager.brain["_mod_file_cache"]
         self._failed_import_hooks = AstroidManager.brain["_failed_import_hooks"]
-        self.always_load_extensions = AstroidManager.brain["always_load_extensions"]
-        self.optimize_ast = AstroidManager.brain["optimize_ast"]
         self.extension_package_whitelist = AstroidManager.brain[
             "extension_package_whitelist"
         ]
         self._transform = AstroidManager.brain["_transform"]
+
+    @property
+    def always_load_extensions(self) -> bool:
+        return AstroidManager.brain["always_load_extensions"]
+
+    @always_load_extensions.setter
+    def always_load_extensions(self, value: bool) -> None:
+        AstroidManager.brain["always_load_extensions"] = value
+
+    @property
+    def optimize_ast(self) -> bool:
+        return AstroidManager.brain["optimize_ast"]
+
+    @optimize_ast.setter
+    def optimize_ast(self, value: bool) -> None:
+        AstroidManager.brain["optimize_ast"] = value
 
     @property
     def register_transform(self):

tests/test_regrtest.py
@@ -9,7 +9,7 @@ from unittest import mock
 
 import pytest
 
-from astroid import MANAGER, Instance, bases, nodes, parse, test_utils
+from astroid import MANAGER, Instance, bases, manager, nodes, parse, test_utils
 from astroid.builder import AstroidBuilder, _extract_single_node, extract_node
 from astroid.context import InferenceContext
 from astroid.exceptions import InferenceError
@@ -36,6 +36,24 @@ class NonRegressionTests(resources.AstroidCacheSetupMixin, unittest.TestCase):
         sys.path.pop(0)
         sys.path_importer_cache.pop(resources.find("data"), None)
 
+    def test_manager_instance_attributes_reference_global_MANAGER(self) -> None:
+        for expected in (True, False):
+            with mock.patch.dict(
+                manager.AstroidManager.brain,
+                values={"always_load_extensions": expected},
+            ):
+                assert (
+                    MANAGER.always_load_extensions
+                    == manager.AstroidManager.brain["always_load_extensions"]
+                )
+            with mock.patch.dict(
+                manager.AstroidManager.brain,
+                values={"optimize_ast": expected},
+            ):
+                assert (
+                    MANAGER.optimize_ast == manager.AstroidManager.brain["optimize_ast"]
+                )
+
     def test_module_path(self) -> None:
         man = test_utils.brainless_manager()
         mod = man.ast_from_module_name("package.import_package_subpackage_module")
@@ -49,9 +67,9 @@ class NonRegressionTests(resources.AstroidCacheSetupMixin, unittest.TestCase):
         self.assertEqual(module.name, "package.subpackage.module")
 
     def test_package_sidepackage(self) -> None:
-        manager = test_utils.brainless_manager()
+        brainless_manager = test_utils.brainless_manager()
         assert "package.sidepackage" not in MANAGER.astroid_cache
-        package = manager.ast_from_module_name("absimp")
+        package = brainless_manager.ast_from_module_name("absimp")
         self.assertIsInstance(package, nodes.Module)
         self.assertTrue(package.package)
         subpackage = next(package.getattr("sidepackage")[0].infer())