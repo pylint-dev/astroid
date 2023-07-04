6469710c7dfa514169b348333a05b0137c0dfdcc
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2023-07-04 21:39:41 +0200

    Bump astroid to 3.0.0a6, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/pylint-dev/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/pylint-dev/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "3.0.0a6-dev0"
+__version__ = "3.0.0a6"
 version = __version__
