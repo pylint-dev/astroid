0361eb460fc92da6ac3c69637cc6a8698b509d88
Jacob Walls <jacobtylerwalls@gmail.com>
2023-08-05 16:38:20 -0400

    Bump astroid to 3.0.0a9, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/pylint-dev/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/pylint-dev/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "3.0.0a9-dev0"
+__version__ = "3.0.0a9"
 version = __version__
