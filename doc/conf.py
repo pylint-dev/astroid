b28b2a1be184b200923581dd836c5a2f2dd5f859
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2023-08-05 16:51:03 +0200

    Update documentation config to use new name for option

--- doc/conf.py
+++ doc/conf.py
@@ -30,7 +30,7 @@ templates_path = ["_templates"]
 source_suffix = ".rst"
 
 # The master toctree document.
-master_doc = "index"
+root_doc = "index"
 
 # General information about the project.
 project = "Astroid"
