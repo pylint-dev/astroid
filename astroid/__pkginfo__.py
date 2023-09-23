29b42e5e9745b172d5980511d14efeac745a5a82
Jacob Walls <jacobtylerwalls@gmail.com>
2023-09-23 15:47:55 -0400

    Bump astroid to 2.15.7, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/PyCQA/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/PyCQA/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "2.15.6"
+__version__ = "2.15.7"
 version = __version__
