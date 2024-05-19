120cb09f12ed527668de9d91d492f65ded60e0b3
Marc Mueller <30130371+cdce8p@users.noreply.github.com>
2024-05-20 00:07:24 +0200

    Bump astroid to 3.2.2, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/pylint-dev/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/pylint-dev/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "3.2.1"
+__version__ = "3.2.2"
 version = __version__
