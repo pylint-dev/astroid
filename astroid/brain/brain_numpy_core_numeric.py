c7ea1e941b4b88ecf0470c9a5e5fd03215f84249
correctmost <134317971+correctmost@users.noreply.github.com>
2024-09-07 14:47:47 -0400

    Consolidate numpy member transforms to reduce function calls

--- astroid/brain/brain_numpy_core_numeric.py
+++ astroid/brain/brain_numpy_core_numeric.py
@@ -8,8 +8,8 @@ import functools
 
 from astroid import nodes
 from astroid.brain.brain_numpy_utils import (
-    attribute_looks_like_numpy_member,
-    infer_numpy_member,
+    attribute_name_looks_like_numpy_member,
+    infer_numpy_attribute,
 )
 from astroid.brain.helpers import register_module_extender
 from astroid.builder import parse
@@ -41,10 +41,11 @@ def register(manager: AstroidManager) -> None:
         manager, "numpy.core.numeric", numpy_core_numeric_transform
     )
 
-    for method_name, function_src in METHODS_TO_BE_INFERRED.items():
-        inference_function = functools.partial(infer_numpy_member, function_src)
-        manager.register_transform(
-            Attribute,
-            inference_tip(inference_function),
-            functools.partial(attribute_looks_like_numpy_member, method_name),
-        )
+    manager.register_transform(
+        Attribute,
+        inference_tip(functools.partial(infer_numpy_attribute, METHODS_TO_BE_INFERRED)),
+        functools.partial(
+            attribute_name_looks_like_numpy_member,
+            frozenset(METHODS_TO_BE_INFERRED.keys()),
+        ),
+    )
