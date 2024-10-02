pylint-dev/astroid

commit dfe1ccce8576fd5854e7eb578985d10f464c1b67

Fix crashes with large positive and negative list multipliers
Closes #2521
Closes #2523

astroid/protocols.py
@@ -142,7 +142,10 @@ def _multiply_seq_by_int(
     context: InferenceContext,
 ) -> _TupleListNodeT:
     node = self.__class__(parent=opnode)
-    if value > 1e8:
+    if value <= 0:
+        node.elts = []
+        return node
+    if len(self.elts) * value > 1e8:
         node.elts = [util.Uninferable]
         return node
     filtered_elts = (

tests/test_protocols.py
@@ -286,6 +286,23 @@ class ProtocolTests(unittest.TestCase):
         element = parsed.inferred()[0].elts[0]
         assert element.value is Uninferable
 
+    @staticmethod
+    def test_uninferable_list_multiplication_with_multiple_operands() -> None:
+        """Attempting to calculate the result is prohibitively expensive."""
+        parsed = extract_node("[0] * 825 * 16547118")
+        element = parsed.inferred()[0].elts[0]
+        assert element.value is Uninferable
+
+    @staticmethod
+    def test_list_multiplication_with_zero_multiplier() -> None:
+        parsed = extract_node("[0] * 0")
+        assert parsed.inferred()[0].elts == []
+
+    @staticmethod
+    def test_list_multiplication_with_negative_multiplier() -> None:
+        parsed = extract_node("[0] * -9223372036854775809")
+        assert parsed.inferred()[0].elts == []
+
 
 def test_named_expr_inference() -> None:
     code = """

commit 091c0d2b58a8c41bbce20bd3542ec6b74086a0fd

Fix OverflowError with empty list and large multiplier
This regressed in dfe1ccce8576.

astroid/protocols.py
@@ -142,7 +142,7 @@ def _multiply_seq_by_int(
     context: InferenceContext,
 ) -> _TupleListNodeT:
     node = self.__class__(parent=opnode)
-    if value <= 0:
+    if value <= 0 or not self.elts:
         node.elts = []
         return node
     if len(self.elts) * value > 1e8:

tests/test_protocols.py
@@ -294,12 +294,17 @@ class ProtocolTests(unittest.TestCase):
         assert element.value is Uninferable
 
     @staticmethod
+    def test_list_multiplication_with_empty_list_and_overflowing_multiplier() -> None:
+        parsed = extract_node("[] * 1163845194457646539560")
+        assert parsed.inferred()[0].elts == []
+
+    @staticmethod
     def test_list_multiplication_with_zero_multiplier() -> None:
         parsed = extract_node("[0] * 0")
         assert parsed.inferred()[0].elts == []
 
     @staticmethod
-    def test_list_multiplication_with_negative_multiplier() -> None:
+    def test_list_multiplication_with_negative_overflowing_multiplier() -> None:
         parsed = extract_node("[0] * -9223372036854775809")
         assert parsed.inferred()[0].elts == []
 