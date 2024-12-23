c3240c05842039baf019470ee05ff6f879bfa735
Daniël van Noord <13665637+DanielNoord@users.noreply.github.com>
2024-12-24 00:35:53 +0100

    Update astroid/interpreter/_import/spec.py
    Co-authored-by: Jacob Walls <jacobtylerwalls@gmail.com>

--- astroid/interpreter/_import/spec.py
+++ astroid/interpreter/_import/spec.py
@@ -169,7 +169,7 @@ class ImportlibFinder(Finder):
 
         # sys.stdlib_module_names was added in Python 3.10
         if PY310_PLUS:
-            # If the module is a stdlib module, check whether this is a frozen module. Note that
+            # If the module name might be a stdlib module, check whether this is a frozen module. Note that
             # `find_spec` actually imports the module, so we want to make sure we only run this code
             # for stuff that can be expected to be frozen. For now this is only stdlib.
             if modname in sys.stdlib_module_names or (
