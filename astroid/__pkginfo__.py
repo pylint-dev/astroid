8de1bb95bd77f038b196ea1bbcb2766b993fe7e4
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-08-06 17:33:39 +0200

    Bump astroid to 4.0.0b1, update changelog

--- astroid/__pkginfo__.py
+++ astroid/__pkginfo__.py
@@ -2,5 +2,5 @@
 # For details: https://github.com/pylint-dev/astroid/blob/main/LICENSE
 # Copyright (c) https://github.com/pylint-dev/astroid/blob/main/CONTRIBUTORS.txt
 
-__version__ = "4.0.0b1-dev1"
+__version__ = "4.0.0b1"
 version = __version__
