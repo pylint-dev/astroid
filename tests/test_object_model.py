330158cea934a536eee828b7028d6dfb9d91ca8e
Casey Jones <731937+doctorcolossus@users.noreply.github.com>
2026-03-08 08:43:57 +0200

    test no crash w/ AnnAssign getattr property fset

--- tests/test_object_model.py
+++ tests/test_object_model.py
@@ -1016,3 +1016,30 @@ def test_builtin_func_no_descriptor_attrs(attr: str) -> None:
     node = builder.extract_node(f"eval.{attr}")
     with pytest.raises(InferenceError):
         next(node.infer())
+
+def test_getattr_on_property_fset_with_annassign_does_not_crash() -> None:
+    """Test that getattr on property fset doesn't crash with AnnAssign."""
+    node = builder.extract_node("""
+    from typing import TypeVar, Generic
+
+    T = TypeVar('T')
+
+    class Base:
+        _x: object
+        @property
+        def x(self):
+            return self._x
+        @x.setter
+        def x(self, val):
+            self._x = val
+
+    class Child(Base, Generic[T]):
+        _x: T
+        @property
+        def x(self) -> T:
+            return self._x
+        @x.setter
+        def x(self, val):
+            getattr(Base.x, "fset", None)  #@
+    """)
+    next(node.infer())
